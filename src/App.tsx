84f63c17e0eb62ac95edb6cc8755cd88db4fef47
Antonio Quaresma <tozequa@gmail.com>
2024-02-19 11:13:02 +0000

    Fetch game genres

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
+import GenreList from "./components/GenreList";
 import GameGrid from "./components/GameGrid";
 
 function App() {
   return (
-    <Grid templateAreas={{
-      // mobile devices
-      base: `"nav" "main"`,
-      // large devices (larger than 1024px)
-      lg: `"nav nav" "aside main"`  
-    }}>
+    <Grid
+      templateAreas={{
+        // mobile devices
+        base: `"nav" "main"`,
+        // large devices (larger than 1024px)
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
       <GridItem area="nav">
         <NavBar />
       </GridItem>
       {/* only shows on large devices */}
       <Show above="lg">
         <GridItem area="aside">
-          Aside
+          <GenreList />
         </GridItem>
       </Show>
       <GridItem area="main">
