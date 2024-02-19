4ec9dad2ff88a5234660e63c8b6e6ec9abd71330
Antonio Quaresma <tozequa@gmail.com>
2024-02-19 10:22:07 +0000

    Get optimized images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,6 @@
 import { Card, CardBody, Heading, Image, HStack} from '@chakra-ui/react'
 import { Game } from '../hooks/useGames'
+import getCroppedImageUrl from '../services/image-url'
 import PlatformIconList from './PlatformIconList'
 import GameScore from './GameScore'
 
@@ -7,10 +8,18 @@ interface Props {
   game: Game
 }
 
+
+
 const GameCard = ({ game }: Props) => {
+  // const optimized_image = game.background_image.replace(
+  //   "/media/",
+  //   "/media/crop/600/400/"
+  //   );
+
   return (
     <Card borderRadius={10} overflow='hidden'>
-      <Image src={game.background_image} />
+      { <Image src={getCroppedImageUrl(game.background_image)} />}
+      {/* <Image src={optimized_image} /> */}
       <CardBody>
         <Heading fontSize='2xl'>{game.name}</Heading>
         <HStack justifyContent='space-between'>
